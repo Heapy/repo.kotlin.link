a37d0d874eb60d55a5e01a517796a3449fc3ba4e
Ruslan Ibragimov <ruslan@ibragimov.by>
2021-02-03 21:32:08 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm").version("1.4.21")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.undertow:undertow-core:2.2.3.Final")
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "repo.kotlin.link"
