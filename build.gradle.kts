0122263376b889330429db2a95384be9fb3b1bb0
Ruslan Ibragimov <ruslan@heapy.io>
2022-10-13 23:50:33 +0300

    Update, from Fleet!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm").version("1.7.10")
-    kotlin("plugin.serialization").version("1.7.10")
+    kotlin("jvm").version("1.7.20")
+    kotlin("plugin.serialization").version("1.7.20")
     application
 }
 
@@ -14,10 +14,10 @@ repositories {
 }
 
 dependencies {
-    implementation("io.undertow:undertow-core:2.2.17.Final")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.9.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("io.undertow:undertow-core:2.3.0.Final")
+    implementation("ch.qos.logback:logback-classic:1.4.4")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.9.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 }
