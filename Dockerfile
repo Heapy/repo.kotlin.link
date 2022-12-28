0dd9a9113f085d0c9d3254dfa183e0884d94e465
Ruslan Ibragimov <ruslan@heapy.io>
2022-12-29 00:54:01 +0300

    Update dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 # Container with application
-FROM amazoncorretto:17.0.4
+FROM bellsoft/liberica-openjre-alpine:17.0.5
 COPY /build/install/repo /repo
 ENTRYPOINT /repo/bin/repo
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm").version("1.7.20")
-    kotlin("plugin.serialization").version("1.7.20")
+    kotlin("jvm").version("1.7.22")
+    kotlin("plugin.serialization").version("1.7.22")
     application
 }
 
@@ -14,10 +14,10 @@ repositories {
 }
 
 dependencies {
-    implementation("io.undertow:undertow-core:2.3.0.Final")
-    implementation("ch.qos.logback:logback-classic:1.4.4")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.9.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
+    implementation("io.undertow:undertow-core:2.3.2.Final")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
 }
