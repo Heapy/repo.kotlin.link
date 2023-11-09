8508bbd5afc4d6c4e8e45f4d9f25edcc59df11f4
Ruslan Ibragimov <ruslan@heapy.io>
2023-11-09 12:09:02 +0300

    Update Dockerfile and build.gradle.kts
    - Update the base image in Dockerfile to `bellsoft/liberica-openjre-alpine:17.0.9`.
    - Update the version of Kotlin JVM plugin in build.gradle.kts to `1.9.20`.
    - Update the version of Kotlin serialization plugin in build.gradle.kts to `1.9.20`.
    - Update the version of undertow-core dependency to `2.3.10.Final` in build.gradle.kts.
    - Update the version of micrometer-registry-prometheus dependency to `1.11.5` in build.gradle.kts.
    - Update the version of kotlinx-serialization-json dependency to `1.6.0` in build.gradle.kts.
    - Update the version of kotlinx-html-jvm dependency to `0.9.1` in build.gradle.kts.
    - Update the version of junit-jupiter dependency to `5.10.1` in build.gradle.kts.
    - Set the Kotlin language version to `2.0` for all source sets in build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm").version("1.9.0")
-    kotlin("plugin.serialization").version("1.9.0")
+    kotlin("jvm").version("1.9.20")
+    kotlin("plugin.serialization").version("1.9.20")
     application
 }
 
@@ -14,10 +14,19 @@ repositories {
 }
 
 dependencies {
-    implementation("io.undertow:undertow-core:2.3.8.Final")
+    implementation("io.undertow:undertow-core:2.3.10.Final")
     implementation("ch.qos.logback:logback-classic:1.4.11")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.11.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.11.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.9.1")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
 }
+
+kotlin {
+    sourceSets.all {
+        languageSettings {
+            languageVersion = "2.0"
+        }
+    }
+}
+
