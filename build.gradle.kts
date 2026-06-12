aa9475dfa10e9e2515b521a879c552b75ee3bfd2
Ruslan Ibrahimau <ruslan@heapy.io>
2026-06-12 18:26:00 +0300

    Upgrade Kotlin and dependencies to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm").version("2.1.0")
-    kotlin("plugin.serialization").version("2.1.0")
+    kotlin("jvm").version("2.4.0")
+    kotlin("plugin.serialization").version("2.4.0")
     application
 }
 
@@ -14,10 +14,10 @@ repositories {
 }
 
 dependencies {
-    implementation("io.undertow:undertow-core:2.3.18.Final")
-    implementation("ch.qos.logback:logback-classic:1.5.16")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.14.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+    implementation("io.undertow:undertow-core:2.4.1.Final")
+    implementation("ch.qos.logback:logback-classic:1.5.34")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.17.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.12.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:6.1.0")
 }
